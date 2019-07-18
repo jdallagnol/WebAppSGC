929048a4eed1676cea777fdf6515cb9120570464
jdallagnol <joelmirdallagnol@hotmail.com>
2019-07-18 17:15:39 -0300

    Gravação de dados no banco

---
+++ src/SGC.Infrastructure/Data/dbInitializer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using SGC.ApplicationCore.Entity;
+
+namespace SGC.Infrastructure.Data
+{
+    public static class dbInitializer
+    {
+        public static void Initialize(ClienteContext context)
+        {
+            // Se tiver clientes cadastrados, retorna...
+            if (context.Clientes.Any())
+            {
+                return;
+            }
+
+            var clientes = new Cliente[]
+            {
+                new Cliente
+                {
+                    Nome = "Joelmir Luiz Dallagnol",
+                    CPF = "12345678901"
+                }
+            };
+        }
+    }
+}
