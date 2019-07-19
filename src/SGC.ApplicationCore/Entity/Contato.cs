ce5a862f63a209940721de62faee163ef67ede47
jdallagnol <joelmirdallagnol@hotmail.com>
2019-07-19 15:57:39 -0300

    Adiciona configurações de Fluent API para Profissões, Menu e outros.

--- src/SGC.ApplicationCore/Entity/Contato.cs
+++ src/SGC.ApplicationCore/Entity/Contato.cs
@@ -12,10 +12,15 @@ namespace SGC.ApplicationCore.Entity
         }
 
         public int ContatoId { get; set; }
+
         public string Nome { get; set; }
+
         public string Telefone { get; set; }
+
         public string Email { get; set; }
+
         public int ClienteId { get; set; }
+
         public Cliente Cliente { get; set; }
     }
 }
