4011256848112844a3d0cbc9d5d8538d59ff377d
jdallagnol <joelmirdallagnol@hotmail.com>
2019-07-19 11:28:12 -0300

    Configura o banco de dados com Fluent API e adiciona o método Seed (DbInitializer)

--- src/SGC.Infrastructure/Data/dbInitializer.cs
+++ src/SGC.Infrastructure/Data/dbInitializer.cs
@@ -6,7 +6,7 @@ using SGC.ApplicationCore.Entity;
 
 namespace SGC.Infrastructure.Data
 {
-    public static class dbInitializer
+    public static class DbInitializer
     {
         public static void Initialize(ClienteContext context)
         {
@@ -21,9 +21,57 @@ namespace SGC.Infrastructure.Data
                 new Cliente
                 {
                     Nome = "Joelmir Luiz Dallagnol",
-                    CPF = "12345678901"
+                    CPF = "11111111111"
+                },
+
+                new Cliente
+                {
+                    Nome = "Rodrigo Peroza",
+                    CPF = "22222222222"
+                },
+
+                new Cliente
+                {
+                    Nome = "Edson Domenech",
+                    CPF = "33333333333"
                 }
             };
+
+            // Adiciona os Clientes
+            context.AddRange(clientes);
+
+            var contatos = new Contato[]
+            {
+                new Contato
+                {
+                    Nome = "Contato Joelmir",
+                    Email = "joelmir@teste.com",
+                    Telefone = "554935669663",
+                    Cliente = clientes[0]
+                },
+
+                new Contato
+                {
+                    Nome = "Contato Rodrigo",
+                    Email = "rodrigo@teste.com",
+                    Telefone = "554935669751",
+                    Cliente = clientes[1]
+                },
+
+                new Contato
+                {
+                    Nome = "Contato Edson",
+                    Email = "edson@teste.com",
+                    Telefone = "554935669756",
+                    Cliente = clientes[2]
+                }
+            };
+
+            // Adiciona os Contatos
+            context.AddRange(contatos);
+
+            // Salva no contexto e adiciona no Banco
+            context.SaveChanges();
         }
     }
 }
--- src/SGC.UI.Web/Program.cs
+++ src/SGC.UI.Web/Program.cs
@@ -6,7 +6,9 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using SGC.Infrastructure.Data;
 
 namespace SGC.UI.Web
 {
@@ -14,7 +16,29 @@ namespace SGC.UI.Web
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            //CreateWebHostBuilder(args).Build().Run();
+
+            //var host = BuildWebHost(args);
+            var host = CreateWebHostBuilder(args).Build();
+            
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+
+                try
+                {
+                    var context = services.GetRequiredService<ClienteContext>();
+                    DbInitializer.Initialize(context);
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "Um erro ocorreu no método seeding de contexto.");
+                }
+            }
+
+            host.Run();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
