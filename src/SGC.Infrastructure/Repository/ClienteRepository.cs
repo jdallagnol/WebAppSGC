95e0fabb3f40c3c170dfc7f36cae300d6c6fc77f
jdallagnol <joelmirdallagnol@hotmail.com>
2019-07-24 14:52:04 -0300

    Aula 07

---
+++ src/SGC.Infrastructure/Repository/ClienteRepository.cs
@@ -0,0 +1,23 @@
+﻿using SGC.ApplicationCore.Entity;
+using SGC.ApplicationCore.Interfaces.Repository;
+using SGC.Infrastructure.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SGC.Infrastructure.Repository
+{
+    public class ClienteRepository : EFRepository<Cliente>, IClienteRepository
+    {
+        public ClienteRepository(ClienteContext dbContext) : base(dbContext)
+        {
+
+        }
+        public Cliente ObterPorProfissao(int clienteId)
+        {
+            return Buscar(x => x.ProfissoesClientes.Any(p => p.ClienteId == clienteId))
+                .FirstOrDefault();
+        }
+    }
+}
